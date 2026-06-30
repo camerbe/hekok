e1b237ed151e04ec924ae5a1e203e661a1fef47b
tristan <tristanvellozzi@gmail.com>
2026-07-01 01:33:17 +0200

    build A apps

--- apps/nghekok/src/app/app.routes.server.ts
+++ apps/nghekok/src/app/app.routes.server.ts
@@ -5,4 +5,12 @@ export const serverRoutes: ServerRoute[] = [
     path: '**',
     renderMode: RenderMode.Prerender,
   },
+  {
+    path: 'actualites/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'communautes/:slug',
+    renderMode: RenderMode.Server,
+  },
 ];
